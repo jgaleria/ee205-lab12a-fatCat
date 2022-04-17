8966be848c328fd814583f4bdfe018b963cbf6fa
Joshua Galeria <jgaleriacs@gmail.com>
2022-04-16 19:37:17 -1000

    Getters and setters

--- Weight.cpp
+++ Weight.cpp
@@ -11,17 +11,15 @@
 #include "Weight.h"
 #include <iostream>
 
-using namespace std;
-
 //Weight conversions
 const float UNKNOWN_WEIGHT   = -1;
 const float KILOS_IN_A_POUND = 0.453592;
 const float SLUGS_IN_A_POUND = 0.031081;
 
 //Unit labels
-const string Weight::POUND_LABEL = "Pound";
-const string Weight::KILO_LABEL  = "Kilo" ;
-const string Weight::SLUG_LABEL  = "Slug" ;
+const std::string Weight::POUND_LABEL = "Pound";
+const std::string Weight::KILO_LABEL  = "Kilo" ;
+const std::string Weight::SLUG_LABEL  = "Slug" ;
 
 ///////////////////
 ///Constructors
@@ -34,5 +32,103 @@ Weight::Weight() noexcept {
     unitOfWeight = POUND;
     weight = UNKNOWN_WEIGHT;
     MaxWeight = UNKNOWN_WEIGHT;
+};
+
+// Only adding weight
+Weight::Weight( float newWeight ) {
+    bIsKnown = false;
+    bHasMax  = false;
+    unitOfWeight = POUND;
+    weight = newWeight;
+    MaxWeight = UNKNOWN_WEIGHT;
 }
 
+// Defaulting the unit of weight
+Weight::Weight( Weight::UnitOfWeight newUnitOfWeight ) noexcept {
+    bIsKnown = true;
+    bHasMax  = false;
+    unitOfWeight = newUnitOfWeight;
+    weight = UNKNOWN_WEIGHT;
+    MaxWeight = UNKNOWN_WEIGHT;
+}
+
+// Defaulting weight and unit
+Weight::Weight( float newWeight, Weight::UnitOfWeight newUnitOfWeight ) {
+    bIsKnown = true;
+    bHasMax  = false;
+    unitOfWeight = newUnitOfWeight;
+    weight = newWeight;
+    MaxWeight = UNKNOWN_WEIGHT;
+}
+
+// Defaulting weight and max weight
+Weight::Weight( float newWeight, float newMaxWeight ) {
+    bIsKnown = true;
+    bHasMax  = true;
+    unitOfWeight = POUND;
+    weight = newWeight;
+    MaxWeight = newMaxWeight;
+}
+
+//Defaulting unit and max weight
+Weight::Weight( Weight::UnitOfWeight newUnitOfWeight, float newMaxWeight ) {
+    bIsKnown = false;
+    bHasMax  = true;
+    unitOfWeight = newUnitOfWeight;
+    weight = UNKNOWN_WEIGHT;
+    MaxWeight = newMaxWeight;
+}
+
+//Defaulting weight, unit, and max
+Weight::Weight( float newWeight, Weight::UnitOfWeight newUnitOfWeight, float newMaxWeight ) {
+    bIsKnown = true;
+    bHasMax  = true;
+    unitOfWeight = newUnitOfWeight;
+    weight = newWeight;
+    MaxWeight = newMaxWeight;
+}
+
+///////////////////////////
+/// Getters and setters
+//////////////////////////
+bool Weight::isWeightKnown() const noexcept {
+    return bIsKnown;
+}
+
+bool Weight::hasMaxWeight() const noexcept {
+    return bHasMax;
+}
+
+float Weight::getWeight() const noexcept {
+
+    return weight;
+}
+
+//float Weight::getWeight( Weight::UnitOfWeight weightUnits ) const noexcept {
+//    float fullWeight = convertWeight( weight, unitOfWeight, weightUnits )
+//    return fullWeight
+//}
+
+float Weight::getMaxWeight() const noexcept {
+    return MaxWeight;
+}
+
+Weight::UnitOfWeight Weight::getWeightUnit() const noexcept {
+    return unitOfWeight;
+}
+
+void Weight::setWeight( float newWeight ) {
+    weight = newWeight;
+}
+
+void Weight::setWeight( float newWeight, Weight::UnitOfWeight weightUnits ) {
+    weight = newWeight;
+    unitOfWeight = weightUnits;
+}
+
+///////////////////////
+/// Checks and methods
+///////////////////////
+//bool isWeightValid( float checkWeight ) const noexcept {
+//
+//}
--- Weight.h
+++ Weight.h
@@ -10,9 +10,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 #pragma once
 
-#include <iostream>
-
-using namespace std;
+#include <string>
 
 class Weight {
 public:
@@ -25,9 +23,9 @@ public:
     static const float SLUGS_IN_A_POUND;
 
     //Unit labels
-    static const string POUND_LABEL;
-    static const string KILO_LABEL;
-    static const string SLUG_LABEL;
+    static const std::string POUND_LABEL;
+    static const std::string KILO_LABEL;
+    static const std::string SLUG_LABEL;
 
 private:
     //Private member variables
@@ -42,15 +40,29 @@ private:
 public:
     //Constructors
     Weight() noexcept;
-    Weight( float newWeight ) noexcept;
+    Weight( float newWeight );
     Weight( UnitOfWeight newUnitOfWeight ) noexcept;
     Weight( float newWeight, UnitOfWeight newUnitOfWeight );
+    Weight( float newWeight, float newMaxWeight );
     Weight( UnitOfWeight newUnitOfWeight, float newMaxWeight );
     Weight( float newWeight, UnitOfWeight newUnitOfWeight, float newMaxWeight );
 
 public:
     //Getters and setters
-    
+    bool isWeightKnown() const noexcept;
+    bool hasMaxWeight() const noexcept;
+
+    float getWeight() const noexcept;
+//    float getWeight(UnitOfWeight weightUnits ) const noexcept;
+    float getMaxWeight() const noexcept;
+    UnitOfWeight getWeightUnit() const noexcept;
+
+    void setWeight( float newWeight );
+    void setWeight( float newWeight, UnitOfWeight weightUnits );
+
+    bool isWeightValid( float checkWeight ) const noexcept;
+    bool validate() const noexcept;
+    void dump() const noexcept;
 
 
 };
